39b7cc55ce36f9a95d7eb733dedc5e7c22964b6c
keesters <68505174+keesters@users.noreply.github.com>
2020-08-23 21:57:12 -0400

    Update setup.py

--- cbapi/setup.py
+++ cbapi/setup.py
@@ -6,12 +6,7 @@ setup(
     url='https://github.abc.com/abc/myabc',
     description=CrunchBase API for pulling people and organization data',
     long_description=open('README.md').read(),
-    install_requires=[
-        "requests==2.7.0",
-        "SomePrivateLib>=0.1.0",
-        ],
-    dependency_links = [
-     "git+git://github.abc.com/abc/SomePrivateLib.git#egg=SomePrivateLib",
-    ],
+    install_requires=['sys','json','requests','pandas'],
+    dependency_links = ["git+git://github.abc.com/abc/SomePrivateLib.git#egg=SomePrivateLib",],
     include_package_data=True,
 )
